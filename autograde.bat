93efdee965e67c718227bc1aaa2cd3c62cea773e
Afrizal Fikri <afrizalfikri96@yahoo.co.id>
2015-04-07 15:24:42 +0700

    readme

---
+++ autograde.bat
@@ -0,0 +1,4 @@
+g++ bin/grader.cpp -o grader -std=c++0x		# compile main grader
+g++ bin/nodelalizer.cpp -o nodel -std=c++0x	# compile output nodelalizer
+grader > result.txt				# execute grader and echo output to result.txt
+del output & del nodel & del grader & del main	# remove all temporary files
