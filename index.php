131db32636a192f28df591a39755259745101878
icalF <laser.survivor@gmail.com>
2015-12-26 00:41:52 +0700

    ajax add case

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description" content="">
-  <meta name="author" content="">
+  <meta name="description" content="Single file autograder with significant tolerance">
+  <meta name="author" content="Afrizal Fikri">
 
   <title>PTI Grader Site</title>
 
@@ -13,6 +13,8 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" type="text/css">
   <link rel="stylesheet" href="http://cdn.jsdelivr.net/animatecss/2.1.0/animate.min.css" type="text/css">
   <link rel="stylesheet" href="css/custom.css">
+  
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js" charset="utf-8"></script>
 </head>
 
 <body id="page-top">
@@ -22,9 +24,6 @@
       <div class="navbar-header">
         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
           <span class="sr-only">Toggle navigation</span>
-          <!-- <span class="icon-bar"></span> -->
-          <!-- <span class="icon-bar"></span> -->
-          <!-- <span class="icon-bar"></span> -->
         </button>
         <a class="navbar-brand page-scroll" href="#page-top">PTI Grader</a>
       </div>
@@ -51,7 +50,7 @@
   <section id="getting-started">
     <div class="container" id="container-getting-started">
       <h1>Getting Started</h1>
-      If you have understand how PTI Grader works, skip to <a href="#grade">grade</a> section
+      If you have understood how PTI Grader works, skip to <a href="#grade">grade</a> section
     </div>
   </section>
 
@@ -59,7 +58,7 @@
     <div class="container" id="container-grade">
       <h1>Grade Now</h1>
       <form id="ajax-grade" method="post" action="upload.php" enctype="multipart/form-data">
-        <div class="field-control">
+        <div class="case">
           Case #1
           <div class="field">
             <textarea name="input-case-1" id="input-case-1" placeholder="Input case #1" required></textarea>
@@ -67,17 +66,13 @@
           <div class="field">
             <textarea name="output-case-1" id="output-case-1" placeholder="Output case #1" required></textarea>
           </div>
-          <button class="btn btn-primary" type="button" onclick="AddCase()">Add case</button>
         </div>
 
+        <button class="btn btn-primary" type="button" onclick="addCase()">Add case</button>
 
-        <div>
-          <br>
-          <p>Select file to be graded:</p>
-          <input type="file" name="fileToUpload" id="fileToUpload">
-          <br>
-          <button class="btn btn-primary" type="submit" onclick="Grade()">Grade</button>
-        </div>
+        <br><p>Select file to be graded:</p>
+        <input type="file" name="fileToUpload" id="fileToUpload">
+        <br><button class="btn btn-primary" type="submit" onclick="Grade()">Grade</button>
       </form>
 
     <div id="grading-messages"></div>
@@ -88,40 +83,39 @@
   <div class="container" id="container-contact">
     <h1>Any problem? Contact me here</h1>
     <form id="ajax-contact" method="post" action="mailer.php">
-      <div class="field-control">
+      <div>
         <div class="field transparent">
           <input type="text" id="name" name="name" placeholder="Name" required>
         </div>
       </div>
 
-      <div class="field-control">
+      <div>
         <div class="field transparent">
           <input type="email" id="email" name="email" placeholder="Email" required>
         </div>
       </div>
 
-      <div class="field-control">
+      <div>
         <div class="field transparent">
           <textarea id="message" name="message" placeholder="Message" required></textarea>
         </div>
       </div>
 
-      <div class="field transparent">
+      <div class="field">
         <button class="btn btn-primary" type="submit" onclick="Send()">Send</button>
       </div>
     </form>
 
-    <div id="contact-messages"></div>
+    <!-- <div id="contact-messages"></div> -->
   </div>
   </section>
 
   <footer class="footer">
     <div class="container">
-      <p class="text-muted">2015 © Copyright <a href="github.com/icalf">Afrizal Fikri</a></p>
+      <p class="text-muted">2015 © Copyright <a href="http://icalicul.me">Afrizal Fikri</a></p>
     </div>
   </footer>
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js" type="text/javascript" charset="utf-8" async defer></script>
-  <script src="/js/apps.js" type="text/javascript" charset="utf-8" async defer></script>
+  <script type="text/javascript" src="/js/apps.js" charset="utf-8"></script>
 </body>
 </html>
--- js/apps.js
+++ js/apps.js
@@ -1,9 +1,25 @@
-$(function() {
-  // Get the form.
-  var form = $('#ajax-contact');
+// $(function() {
+//   // Get the form.
+//   var form = $('#ajax-contact');
 
-  // Get the messages div.
-  var formMessages = $('#form-messages');
+//   // Get the messages div.
+//   var formMessages = $('#form-messages');
 
-  // TODO: The rest of the code will go here...
-});
+//   // TODO: The rest of the code will go here...
+// });
+
+var addCase = function() {
+  var n = $('.case').length + 1;
+
+  $('#ajax-grade .case:last').append(
+    '<div class="case">'+
+      'Case #'+n+
+      '<div class="field">'+
+        '<textarea name="input-case-'+n+'" id="input-case-'+n+'" placeholder="Input case #'+n+'" required></textarea>'+
+      '</div>'+
+      '<div class="field">'+
+        '<textarea name="output-case-'+n+'" id="output-case-'+n+'" placeholder="Output case #'+n+'" required></textarea>'+
+      '</div>'+
+    '</div>'
+  );
+};
